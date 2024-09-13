2089443f131ed8491fe6158c4f8f06e92e6f624c
berkeracir <berkeracir159@gmail.com>
2024-09-13 19:52:43 +0300

    black reformat

--- orphex/apps.py
+++ orphex/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class OrphexConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'orphex'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "orphex"
--- orphex/urls.py
+++ orphex/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("api", include("orphex.api.urls")),    # TODO(berker) remove
+    path("api", include("orphex.api.urls")),  # TODO(berker) remove
     path("worker", include("orphex.worker.urls")),
 ]
